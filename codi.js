ed35c3c0cb69cf0e518d96a64e26d10f8081e965
Juli <juli.fer14@gmail.com>
2015-10-01 20:29:45 +0200

    Pujada

---
+++ codi.js
@@ -0,0 +1,18 @@
+function carregarCategories() {
+	//Crear objecte xmlhttp
+	var xmlhttp;
+	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp = new XMLHttpRequest();
+	} else {// code for IE6, IE5
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			document.getElementById("catProductes").innerHTML = xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("GET", "carregarCategories.php", true);
+	xmlhttp.send(null);
+
+
+}
